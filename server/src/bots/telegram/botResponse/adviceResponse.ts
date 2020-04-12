40e647f78bea59b61f8a8aea4c409b6f23e995a0
Danylo Bilokha <danbilokha@gmail.com>
2020-04-12 19:30:23 +0300

    update list of contributors; add useful videos

--- server/src/bots/telegram/botResponse/adviceResponse.ts
+++ server/src/bots/telegram/botResponse/adviceResponse.ts
@@ -1,17 +1,19 @@
 import {
     alternativeGreetings,
     encouragingMessage,
+    getCovid19ExplanationVideo,
     socialDistancing,
     suggestedBehaviors
-} from '../../../messages/userMessage';
+} from "../../../messages/feature/adviceMessages";
 
 export const showAdvicesHowToBehaveResponse = (bot, message, chatId): Promise<void> => {
     return bot.sendMessage(
         chatId,
-        `ℹ Suggested Behaviors
+        `ℹ Suggested Behaviors for ${getCovid19ExplanationVideo()}
         ${suggestedBehaviors()} \nℹ Social Distancing
         ${socialDistancing()} \nℹ Alternative Greetings
         ${alternativeGreetings()} \n${encouragingMessage()}
-        `
+        `,
+        {parse_mode: 'HTML'}
     );
 };
--- server/src/messages/userMessage.ts
+++ server/src/messages/userMessage.ts
@@ -7,31 +7,3 @@ export const getUserName = ({first_name, last_name, username}): string => {
 export const greetUser = (from): string => {
     return `Hi, ${getUserName(from)}. ${FIRST_MESSAGE_EXPLANAITON}`;
 };
-
-export const encouragingMessage = (): string => `Wash 🧼 your hands 👏 and stay healthy! Everything will be OK`;
-
-export const suggestedBehaviors = (): string => `
-🚫🤦 Don't touch your face
-🚫🤧🤲 Don't sneeze into hands
-✅🤧💪 Do sneeze into your elbow
-🧼🖐⏲2️⃣0️⃣  Wash your hands regularly, for at least 20 seconds
-✅📦😌 If practical, have groceries and other items delivered to your home
-🚫🛒😡 When using local shops, don't buy literally everything on the shelves. Leave enough for others 💕
-`;
-
-export const socialDistancing = (): string => `
-🚫🤝 No handshakes
-🚫🧑‍🤝‍🧑 No close contact
-🚫🏟 No large gatherings
-`;
-
-export const alternativeGreetings = (): string => `
-👋 Waving Hand – Hello
-🖖 Vulcan Salute – Live long and prosper
-✌️ Victory Hand – Peace
-🤟 Love-You Gesture – I love you in American Sign Language
-🤘 Sign of the Horns – Rock on
-💪 Flexed Biceps – Elbow-touch
-🙏 Folded Hands – Namaste
-✋💨🤚 – Air High Five
-`;
