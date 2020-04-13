5776d6d8e2816c779cf6f9f846dbcbfab0455b25
Danylo Bilokha <danbilokha@gmail.com>
2020-04-13 21:48:38 +0300

    add jest rule

--- server/src/routes/base/base.ts
+++ server/src/routes/base/base.ts
@@ -4,7 +4,7 @@ const pkg = require(__dirname + '/../../../../package.json');
 
 export let base = (req: Request, res: Response) => {
     res.json({
-        message: 'Welcome to API.',
+        message: 'Welcome to Covid19 live bot api. Refer to documentation here - https://github.com/danbilokha/covid19liveupdates',
         version: pkg.version || 'Unkown',
         containerVersion: process.env.CONTAINER_VERSION
     });
