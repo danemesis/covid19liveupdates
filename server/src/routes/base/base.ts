2436bd3dce9b1c78a315439cd994dbc186f4e152
Danylo Bilokha <danbilokha@gmail.com>
2020-04-13 20:48:45 +0300

    works

--- server/src/routes/base/base.ts
+++ server/src/routes/base/base.ts
@@ -6,6 +6,6 @@ export let base = (req: Request, res: Response) => {
     res.json({
         message: 'Welcome to API.',
         version: pkg.version || 'Unkown',
-        containerVersion : process.env.CONTAINER_VERSION
+        containerVersion: process.env.CONTAINER_VERSION
     });
 };
