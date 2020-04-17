80645a44f3fbc2078f8bbba9b70e6bbdf709d3b6
Danylo Bilokha <danbilokha@gmail.com>
2020-04-17 10:04:46 +0300

    add simple message for container info

--- server/src/routes/base/base.ts
+++ server/src/routes/base/base.ts
@@ -6,6 +6,6 @@ export let base = (req: Request, res: Response) => {
     res.json({
         message: 'Welcome to Covid19 live bot api. Refer to documentation here - https://github.com/danbilokha/covid19liveupdates',
         version: pkg.version || 'They didn\'n say me :c',
-        containerVersion: process.env.CONTAINER_VERSION || 'They didn\'n say me :c',
+        containerVersion: process.env.CONTAINER_VERSION || 'They didn\'n say me :c But it should be the same as package.version',
     });
 };
